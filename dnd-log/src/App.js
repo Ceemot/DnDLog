435b4f05b50be268e76574d727443bbd8d89712a
xzantim <xzantim@gmail.com>
2024-07-16 23:18:33 +0100

    text change

--- dnd-log/src/App.js
+++ dnd-log/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Check out ym change boiiii
         </p>
         <a
           className="App-link"
