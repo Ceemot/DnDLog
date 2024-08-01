c51e1e7f5956a90ed4ea12096b1ce3fc99babfdb
xzantim <xzantim@gmail.com>
2024-08-01 23:20:23 +0100

    added styling, output window to display generated record json

--- dnd-log/src/Components/AddLog.tsx
+++ dnd-log/src/Components/AddLog.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { useState } from "react";
+import { Log } from "../Components/Types";
 
 export default function AddLog() {
   const [formData, setFormData] = useState({
     title: "",
     location: "",
-    entities: [""],
+    entities: "",
     description: "",
   });
+
+  const [outputText, setOutputText] = useState("");
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -16,17 +19,25 @@ export default function AddLog() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(
-      `Title: ${formData.title}, Location: ${formData.location}, Entities: ${formData.entities}, Description: ${formData.description}`
-    );
+    var logItem: Log = {
+      LogId: 3,
+      Title: formData.title,
+      Location: formData.location,
+      Entities: formData.entities.split(","),
+      Description: formData.description,
+    };
+    setOutputText(JSON.stringify(logItem, undefined, 4));
   };
   //Was not sure how to make an array in entities, was thinking a checkbox for party members
   //Also the css on this page is whack
   return (
     <div style={{ marginBlockStart: "0px", padding: "40px" }}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <label className="formLabel" htmlFor="title">
+          Title:
+        </label>
         <input
+          className="formInput"
           type="text"
           id="title"
           name="title"
@@ -35,8 +46,11 @@ export default function AddLog() {
         />
         <br />
         <br />
-        <label htmlFor="location">Location:</label>
+        <label className="formLabel" htmlFor="location">
+          Location:
+        </label>
         <input
+          className="formInput"
           type="text"
           id="location"
           name="location"
@@ -45,8 +59,11 @@ export default function AddLog() {
         />
         <br />
         <br />
-        <label htmlFor="entities">Entities:</label>
+        <label className="formLabel" htmlFor="entities">
+          Entities:
+        </label>
         <input
+          className="formInput"
           type="text"
           id="entities"
           name="entities"
@@ -55,8 +72,11 @@ export default function AddLog() {
         />
         <br />
         <br />
-        <label htmlFor="description">Description:</label>
+        <label className="formLabel" htmlFor="description">
+          Description:
+        </label>
         <textarea
+          className="formInput textArea"
           id="description"
           name="description"
           value={formData.description}
@@ -66,6 +86,15 @@ export default function AddLog() {
         <br />
         <button type="submit">Submit</button>
       </form>
+      <div>
+        <textarea
+          id="outputText"
+          name="outputText"
+          className="theOutput"
+          value={outputText}
+          readOnly
+        />
+      </div>
     </div>
   );
 }
