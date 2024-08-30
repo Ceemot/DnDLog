1584643bfcdd9f171e1c7487be592f80a0f5982d
Ceemot <thecmot@gmail.com>
2024-08-30 20:56:17 +0100

    Switched over to MUI collapse WIP

--- dnd-log/src/Components/Logs.tsx
+++ dnd-log/src/Components/Logs.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import * as data from "../Content/Logs.json";
 import Collapsible from "react-collapsible";
 import DescriptionText from "./DescriptionText.tsx";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
 
 export default function LogBookComponent() {
   const records = JSON.parse(JSON.stringify(data));
@@ -14,22 +18,42 @@ export default function LogBookComponent() {
   );
 }
 
+// function LogListItem(props) {
+//   const record = props.record;
+//   return (
+//     <div
+//       className="Log-item-active"
+//       style={{ padding: "10px", textAlign: "center" }}
+//     >
+//       <Collapsible
+//         trigger={
+//           <h3>
+//             {record.title} : {record.location}
+//           </h3>
+//         }
+//       >
+//         <DescriptionText description={record.description} />
+//       </Collapsible>
+//     </div>
+//   );
+// }
+
 function LogListItem(props) {
   const record = props.record;
   return (
-    <div
-      className="Log-item-active"
-      style={{ padding: "10px", textAlign: "center" }}
-    >
-      <Collapsible
-        trigger={
-          <h3>
-            {record.title} : {record.location}
-          </h3>
-        }
-      >
-        <DescriptionText description={record.description} />
-      </Collapsible>
+    <div>
+      <Accordion>
+        <AccordionSummary aria-controls="panel-content" id="panel-header">
+          <Typography align="center" sx={{ width: "100%" }}>
+            {record.title} : {record.location}{" "}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography align="center" sx={{ width: "100%" }}>
+            {record.description}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
